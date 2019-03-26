c1cfd979d221cd31bc65d91ee74f465576227c81
Nick Young <youngnicholas228@gmail.com>
2019-03-26 15:13:33 -0700

    A

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ app.get('/', function(req, res) {
 	res.send("hello")
 })
 
+app.get('/user', function(req, res) {
+	res.render("../index.html")
+	//res.sendFile(path.join(__dirname+'../index.html'))
+})
+
+app.get('*', function(req, res) {
+	res.send("404 not found")
+})
+
 app.listen(port, function(){
 	console.log("haha yeet")
 })
