15b6f3fa6808a40c43d16b825548d00560c0793d
Nick Young <youngnicholas228@gmail.com>
2019-03-28 00:13:06 -0400

    adding packages

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
 const port = 3000
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get('/', function(req, res) {
-	res.send("hello")
+	res.render("index.ejs");
 })
 
 app.get('/user', function(req, res) {
-	res.render("../index.html")
-	res.sendFile(path.join('/index.html'))
+	res.render("user.ejs");
+})
+
+app.get('/grace', function(req, res) {
+	res.render('grace.ejs');
+})
+
+app.get('/payment', function(req, res) {
+	res.render("payment.ejs");
 })
 
 app.get('*', function(req, res) {
-	res.send("404 not found")
+	res.redirect('/');
 })
 
 app.listen(port, function(){
-	console.log("haha yeet")
+	console.log("Server listening on port: " + port);
 })
