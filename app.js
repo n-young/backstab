64e918aa0ea928c594dc42778106bd2aa7222069
Nick Young <youngnicholas228@gmail.com>
2019-04-17 10:58:09 -0700

    user model

--- app.js
+++ app.js
@@ -7,25 +7,30 @@ const bodyParser = require('body-parser');
 const User = require('./models/user.js')
 const LocalStrategy = require('passport-local');
 const passportLocalMongoose = require('passport-local-mongoose');
-const port = process.env.PORT;
+//const port = process.env.PORT;
+const port = 3000;
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://n-young:tetris101@spyvspy-ogfdo.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, {
+    useNewUrlParser: true
+});
 client.connect(err => {
-  const collection = client.db("spyvspy").collection("devices");
-  client.close();
+    const collection = client.db("spyvspy").collection("devices");
+    client.close();
 });
 mongoose.connect(uri);
 
 app.use(express.static("public"));
 app.use(require("express-session")({
-	secret:"my goodness this code is very janky",
-	resave: false,
-	saveUninitialized: false
+    secret: "my goodness this code is very janky",
+    resave: false,
+    saveUninitialized: false
 }));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -38,75 +43,96 @@ passport.deserializeUser(User.deserializeUser());
 //=========================================
 
 app.get('/', function(req, res) {
-	if(req.isAuthenticated()){
-		return res.redirect("/user");
-	}
-	res.render("index.ejs");
+    if (req.isAuthenticated()) {
+        return res.redirect("/user");
+    }
+    res.render("index.ejs");
 })
 
 app.get('/payment', isLoggedIn, function(req, res) {
-	res.render("payment.ejs", {currentUser: req.user});
+    res.render("payment.ejs", {
+        currentUser: req.user
+    });
 })
 
 app.get('/grace', isLoggedIn, isPaid, function(req, res) {
-	res.render('grace.ejs', {currentUser: req.user});
+    res.render('grace.ejs', {
+        currentUser: req.user
+    });
 })
 
 app.get('/user', isGameBegun, isLoggedIn, isPaid, function(req, res) {
-	res.render("user.ejs", {currentUser: req.user});
+    res.render("user.ejs", {
+        currentUser: req.user
+    });
+})
+
+app.get('/nickyoungpage', function(req, res) {
+    res.render("user.ejs", {
+        currentUser: req.user
+    });
 })
 
 //REGISTRATION
 
 app.post('/register', function(req, res) {
-	User.register(new User({username: req.body.username, name: req.body.name, number: req.body.number, paid: false, target: "none"}), req.body.password, function(err, user){
-		if(err) {
-			console.log(err);
-			return res.render('error.ejs');
-		}
-		passport.authenticate("local")(req, res, function() {
-			res.redirect("/grace");
-		})
-	});
+    User.register(new User({
+        email: req.body.username,
+        name: req.body.name,
+        id: req.body.number,
+        paid: false,
+        target: "none",
+		target-id: 0,
+		due: new Date(),
+		alive: true
+
+    }), req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render('error.ejs');
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/grace");
+        })
+    });
 });
 
 app.post('/login', passport.authenticate("local", {
-	successRedirect: "/grace",
-	failureRedirect: "/"
-}), function(req, res) {
-})
+    successRedirect: "/grace",
+    failureRedirect: "/"
+}), function(req, res) {})
 
-app.get("/logout", function(req, res){
-	req.logout();
-	res.redirect("/")
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/")
 });
 
 function isGameBegun(req, res, next) {
-	gameBegun = false;
-	if(gameBegun){
-		res.redirect("/user");
-	}
-	res.redirect("/grace");
+    gameBegun = false;
+    if (gameBegun) {
+        res.redirect("/user");
+    }
+    res.redirect("/grace");
 }
 
 function isLoggedIn(req, res, next) {
-	if(req.isAuthenticated()){
-		return next();
-	}
-	res.redirect("/");
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/");
 }
 
 function isPaid(req, res, next) {
-	if(req.user.paid === true) {
-		return next();
-	}
-	res.redirect("/payment");
+    if (req.user.paid === true) {
+        return next();
+    }
+    res.redirect("/payment");
 }
 
 app.get('*', function(req, res) {
-	res.redirect('/');
+    res.redirect('/');
 })
 
-app.listen(port, function(){
-	console.log("Server listening on port: " + port);
+app.listen(port, function() {
+    console.log("Server listening on port: " + port);
 })
--- models/user.js
+++ models/user.js
@@ -2,12 +2,15 @@ var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose')
 
 var UserSchema = new mongoose.Schema({
-	username: String,
+	email: String,
 	name: String,
-	number: Number,
+	id: Number,
 	password: String,
 	paid: Boolean,
-	target: String
+	target: String,
+	target-id: Number,
+	due: Date,
+	alive: Boolean
 });
 
 UserSchema.plugin(passportLocalMongoose);
